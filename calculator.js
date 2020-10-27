db7b5b0b927815c631468bf549650c4fb8b6a7f3
Andrzej Dolinski <andrzej.dolinski@PL-MAC-C02C803XLVDM.local>
2020-10-27 23:15:54 +0100

    displaying hardcoded value, reacting for each button click

--- calculator.js
+++ calculator.js
@@ -1,10 +1,20 @@
-const standardButton = document.querySelector('.standard-button');
-const display = document.querySelector('.display-row');
+const standardButton = document.querySelectorAll(".standard-button");
+const display = document.querySelector(".display-row");
+let result = 54;
 
-standardButton.addEventListener('click', function() {
-    let buttonValue = standardButton.textContent;
-    if (standardButton.classList.contains('standard-button')) {
-        console.log(`${buttonValue}`);
-        display.innerHTML = `${buttonValue}`;
-    }
-});
+for (i = 0; i < 17; i++) {
+  if (!isNaN(parseInt(`${standardButton[i].innerText}`))) {
+    console.log(`${standardButton[i].innerText}`);
+  } else console.log(`tylek`);
+}
+
+for (i = 0; i < standardButton.length; i++) {
+  standardButton[i].addEventListener("click", function () {
+    if (!isNaN(parseInt(`${standardButton[i].innerText}`))) {
+      console.log(`${standardButton[i].innerText}`);
+      display.innerText = standardButton[i].innerText;
+    } else console.log(`tylek`);
+  });
+}
+
+display.innerHTML = result;
